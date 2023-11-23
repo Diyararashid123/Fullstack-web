14186745da459d70027f74c0e223e4f6653ac4c0
Diyara <diyararashid@gmail.com>
2023-11-23 03:53:55 +0100

    API isuess

--- src/routes/api/order-webhook/+server.js
+++ src/routes/api/order-webhook/+server.js
@@ -1,35 +1,17 @@
-// src/routes/api/order/[orderId].js
 import { dbClient } from '$lib/server/db.js';
 import { ordersTable } from '$lib/server/schema.js';
 
-export async function GET({ params }) {
-    const { orderId } = params;
-
-    try {
-        // Fetch the order from the database
+export async function GET() {
+   
+        // Fetch all orders from the database
         const orders = await dbClient
-            .select()
+            .select('*')
             .from(ordersTable)
-            .where(ordersTable.orderId.eq(orderId))
             .execute();
 
-        const order = orders[0]; // Assuming the first result is the order we want
-
-        if (!order) {
-            throw new Error('Order not found');
-        }
-
-        return new Response(JSON.stringify({
-            orderId: orderId,
-            processed: order.processed
-        }), {
+        return new Response(JSON.stringify(orders), {
             headers: { 'Content-Type': 'application/json' },
             status: 200
         });
-    } catch (error) {
-        return new Response(JSON.stringify({ error: error.message }), {
-            headers: { 'Content-Type': 'application/json' },
-            status: 500
-        });
-    }
+    
 }
