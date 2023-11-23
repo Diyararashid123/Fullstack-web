e2f2fc70ee8f84cdeffd8a75a7702020709a993e
Diyara <diyararashid@gmail.com>
2023-11-23 08:37:41 +0100

    conenction to the order page

--- src/routes/api/order-webhook/+server.js
+++ src/routes/api/order-webhook/+server.js
@@ -1,17 +1,15 @@
-// Import Supabase client
+
 import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_URL,SUPABASE_KEY } from "$env/static/private";
-// Initialize Supabase client
+
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-// GET request handler
-// GET request handler
 export async function GET({ query }) {
     try {
-        // Extract the user ID from the query parameter
+        
         const userId = query.get('userId');
 
-        // Check if there are any processed orders for the specific user
+     
         let { data: orders, error } = await supabase
             .from('order')
             .select('processed')
@@ -22,7 +20,7 @@ export async function GET({ query }) {
             throw error;
         }
 
-        // Determine if any orders are processed
+  
         const isOrderProcessed = orders.length > 0;
 
         return new Response(JSON.stringify({ isOrderProcessed }), {
@@ -30,7 +28,7 @@ export async function GET({ query }) {
             status: 200
         });
     } catch (err) {
-        // Handle any errors that occur during the fetch
+       
         return new Response(JSON.stringify({ error: err.message }), {
             headers: { 'Content-Type': 'application/json' },
             status: 500
@@ -38,21 +36,18 @@ export async function GET({ query }) {
     }
 }
 
-// POST request handler
+
 export async function POST(request) {
     try {
-        // Parse the incoming webhook data
+       
         const webhookData = await request.json();
 
-        // Process the webhook data
         console.log('Webhook Data:', webhookData);
 
-        // Here, you can add logic based on the webhook data received
-        // For example, update the status of an order, trigger other processes, etc.
 
         return new Response(null, { status: 200 });
     } catch (err) {
-        // Handle any errors that occur
+     
         return new Response(JSON.stringify({ error: err.message }), {
             headers: { 'Content-Type': 'application/json' },
             status: 500
