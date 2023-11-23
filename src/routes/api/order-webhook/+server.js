fe6cd8c372d448a58797da341fae779fd1480f4f
Diyara <diyararashid@gmail.com>
2023-11-23 05:39:08 +0100

    API isuess

--- src/routes/api/order-webhook/+server.js
+++ src/routes/api/order-webhook/+server.js
@@ -1,13 +1,15 @@
 // Import the database client from your db.js file
 import { dbClient } from '$lib/server/db.js';
+import { ordersTable } from '$lib/server/schema.js';
 
 // GET request handler
 export async function GET() {
     try {
-        // Directly query the 'order' table, using double quotes because 'order' is a reserved keyword
-        const orders = await dbClient.sql`
-            SELECT * FROM "order"
-        `;
+        // Fetch new orders from the database that haven't been processed yet
+        const orders = await dbClient.query(ordersTable.select('*').where('processed', false));
+
+        // Optionally, you can update these orders as processed in a similar way as your Python script
+        // ...
 
         return new Response(JSON.stringify(orders), {
             headers: { 'Content-Type': 'application/json' },
