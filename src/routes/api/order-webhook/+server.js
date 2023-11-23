c48006e1613fd8c8439998ef28e25a4018b933a1
Diyara <diyararashid@gmail.com>
2023-11-23 03:41:31 +0100

    API isuess

--- src/routes/api/order-webhook/+server.js
+++ src/routes/api/order-webhook/+server.js
@@ -1,19 +1,35 @@
 // src/routes/api/order/[orderId].js
+import { dbClient } from '$lib/server/db.js'; // Adjust the path as necessary
+import { ordersTable } from '$lib/server/schema.js';
 
 export async function GET({ params }) {
     const { orderId } = params;
-    console.log("Received request for order ID:", orderId);
 
-    // Mock processed status for testing
-    // Set this to true or false as per your test scenario
-    const isProcessed = false;
+    try {
+        // Fetch the order from the database
+        const order = await dbClient
+            .select(ordersTable.processed)
+            .from(ordersTable)
+            .where(ordersTable.orderId.eq(orderId))
+            .execute()
+            .then((result) => result[0]);
 
-    // Return only the processed status
-    return new Response(JSON.stringify({
-        orderId: orderId,
-        processed: isProcessed
-    }), {
-        headers: { 'Content-Type': 'application/json' },
-        status: 200
-    });
+        if (!order) {
+            throw new Error('Order not found');
+        }
+
+        // Return the processed status of the order
+        return new Response(JSON.stringify({
+            orderId: orderId,
+            processed: order.processed
+        }), {
+            headers: { 'Content-Type': 'application/json' },
+            status: 200
+        });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: error.message }), {
+            headers: { 'Content-Type': 'application/json' },
+            status: 500
+        });
+    }
 }
