8c1a0f6174a1966a5df6b10f71746bf2166649bc
Diyara <diyararashid@gmail.com>
2023-11-23 03:24:26 +0100

    API isuess

--- src/routes/api/order-webhook/+server.js
+++ src/routes/api/order-webhook/+server.js
@@ -1,27 +1,25 @@
 // src/routes/api/order/[orderId].js
 
-// Import your existing database client setup here
-import { dbClient } from '$lib/dbClient.js';
-
-
 export async function GET({ params }) {
     const { orderId } = params;
-    console.log("i was here");
-        const { data: order, error } = await dbClient
-            .from('orders')
-            .select('*')
-            .eq('id', orderId)
-            .single();
-                console.log(data);
+    console.log("Received request for order ID:", orderId);
 
-        // Return the order data and its processed status
-        return new Response(JSON.stringify({
-            order: order,
-            processed: order ? order.processed : false,
-            message: "ITS RETURNED"
-        }), {
-            headers: { 'Content-Type': 'application/json' },
-            status: 200
-        });
-    } 
+    // Mock order data for testing
+    const mockOrder = {
+        id: orderId,
+        // Add other mock fields as necessary
+    };
 
+    // Mock processed status
+    const isProcessed = false; // Set this to true or false for testing
+
+    // Return the mock order data and its processed status
+    return new Response(JSON.stringify({
+        order: mockOrder,
+        processed: isProcessed,
+        message: "Mock order data returned"
+    }), {
+        headers: { 'Content-Type': 'application/json' },
+        status: 200
+    });
+}
