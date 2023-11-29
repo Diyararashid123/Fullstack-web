1ff45830379a193ac0b942439448e45ffb039f15
Diyara <diyararashid@gmail.com>
2023-11-29 13:52:53 +0100

    Processed order fixed

--- src/lib/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// src/lib/supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://wolrwotpqfgrxtgpjljx.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvbHJ3b3RwcWZncnh0Z3BqbGp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk2NDQzNDMsImV4cCI6MjAxNTIyMDM0M30.gRkJ0nzQFEKciaVLRCD9aMWc-2Ufb6GYKP5RvM_1bW8'; // Replace with your 
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- src/routes/api/order-api/+page.server.js
+++ src/routes/api/order-api/+page.server.js
@@ -1,14 +1,12 @@
-
 import { dbClient } from '../../../lib/server/db.js';
 import { processedOrdersTable } from '../../../lib/server/schema.js';
-
-
+import { eq } from "drizzle-orm";
 
 export const load = async ({ locals }) => {
-  const orderprocessed = await dbClient.select().from(processedOrdersTable);
-
+  // Validate the user session
   const session = await locals.auth.validate();
   if (!session) {
+    // If there is no valid session, return a 401 status with an empty order list
     return {
       status: 401,
       body: {
@@ -16,6 +14,17 @@ export const load = async ({ locals }) => {
       }
     };
   }
+
+
+  const userId = session.user.userId;
+
+
+  const orderprocessed = await dbClient
+    .select()
+    .from(processedOrdersTable)
+    .where(eq(processedOrdersTable.userId, userId)) 
+    .execute(); 
+
 
   return {
     body: {
